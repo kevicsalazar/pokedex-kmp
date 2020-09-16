3cd4e3049132db2a2e6953290253374b4212a878
Kevin Salazar <kevicsalazar1994@gmail.com>
2020-09-16 10:45:04 -0500

    Avance MVVM 4

--- shared/src/iosMain/kotlin/com/kevicsalazar/pokedex/shared/features.arch/livedata/LiveData.kt
+++ shared/src/iosMain/kotlin/com/kevicsalazar/pokedex/shared/features.arch/livedata/LiveData.kt
@@ -1,22 +1,23 @@
 package com.kevicsalazar.pokedex.shared.features.arch.livedata
 
-actual open class LiveData<T>(initialValue: T) {
-    private var storedValue: T = initialValue
-    private val observers = mutableListOf<(T) -> Unit>()
+actual open class LiveData<T> {
 
-    actual open val value: T
+    private var storedValue: T? = null
+    private val observers = mutableListOf<(T?) -> Unit>()
+
+    actual open val value: T?
         get() = storedValue
 
-    actual fun addObserver(observer: (T) -> Unit) {
+     fun addObserver(observer: (T?) -> Unit) {
         observer(value)
         observers.add(observer)
     }
 
-    actual fun removeObserver(observer: (T) -> Unit) {
+     fun removeObserver(observer: (T?) -> Unit) {
         observers.remove(observer)
     }
 
-    protected fun changeValue(value: T) {
+    protected fun changeValue(value: T?) {
         storedValue = value
 
         observers.forEach { it(value) }
