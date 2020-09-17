87e6f3c47d478917ef5aada8bef66ec91e755521
Kevin Salazar <kevicsalazar1994@gmail.com>
2020-09-17 11:34:47 -0500

    Avance MVVM 10 iosApp

---
+++ iosApp/iosApp/PokemonListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PokemonListViewController.swift
+//  iosApp
+//
+//  Created by Kevin Salazar on 9/16/20.
+//  Copyright © 2020 orgName. All rights reserved.
+//
+
+import UIKit
+import shared
+
+class PokemonListViewController: UIViewController {
+
+    let viewModel = Injector.init().providePokemonListViewModel()
+    
+    @IBOutlet weak var name: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        observeViewModel()
+        
+        viewModel.getPokemonList()
+        
+    }
+
+    func observeViewModel() {
+        viewModel.viewState.addObserver { (state) in
+            if (state is PokemonListViewState.Success) {
+                //self.name.text = "SUCCESS"
+            } else if (state is PokemonListViewState.Loading) {
+                //self.name.text = "Loading"
+            } else if (state is PokemonListViewState.Error) {
+                //self.name.text = "ERROR"
+            }
+            
+        }
+    }
+    
+}
