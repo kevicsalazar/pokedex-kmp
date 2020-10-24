00b6d7b8e7c2544a4d2d4712ff3e6faafd077548
Kevin Salazar <kevicsalazar1994@gmail.com>
2020-10-24 12:05:33 -0500

    Fix: UI iOS

--- shared/src/iosMain/kotlin/com/kevicsalazar/pokedex/shared/features.arch/UI.kt
+++ shared/src/iosMain/kotlin/com/kevicsalazar/pokedex/shared/features.arch/UI.kt
@@ -1,22 +1,58 @@
 package com.kevicsalazar.pokedex.shared.features.arch
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Runnable
-import platform.darwin.dispatch_async
-import platform.darwin.dispatch_get_main_queue
-import platform.darwin.dispatch_queue_t
+import kotlinx.coroutines.*
+import platform.darwin.*
 import kotlin.coroutines.CoroutineContext
 
 actual val Dispatchers.UI: CoroutineDispatcher
-    get() = NsQueueDispatcher(dispatch_get_main_queue())
+    get() = UIDispatcher()
 
-internal class NsQueueDispatcher(
-    private val dispatchQueue: dispatch_queue_t
-) : CoroutineDispatcher() {
+@OptIn(InternalCoroutinesApi::class)
+internal class UIDispatcher : CoroutineDispatcher(), Delay {
+    private val mQueue = dispatch_get_main_queue()
+
     override fun dispatch(context: CoroutineContext, block: Runnable) {
-        dispatch_async(dispatchQueue) {
+        dispatch_async(mQueue) {
             block.run()
+        }
+    }
+
+    override fun scheduleResumeAfterDelay(
+        timeMillis: Long,
+        continuation: CancellableContinuation<Unit>
+    ) {
+        dispatch_after(
+            `when` = dispatch_time(
+                DISPATCH_TIME_NOW,
+                timeMillis * NSEC_PER_MSEC.toLong()
+            ),
+            queue = mQueue
+        ) {
+            val result = continuation.tryResume(Unit)
+            if (result != null) {
+                continuation.completeResume(result)
+            }
+        }
+    }
+
+    @ExperimentalUnsignedTypes
+    override fun invokeOnTimeout(timeMillis: Long, block: Runnable): DisposableHandle {
+        var disposed = false
+        dispatch_after(
+            `when` = dispatch_time(
+                DISPATCH_TIME_NOW,
+                timeMillis * NSEC_PER_MSEC.toLong()
+            ),
+            queue = mQueue
+        ) {
+            if (disposed) return@dispatch_after
+
+            block.run()
+        }
+        return object : DisposableHandle {
+            override fun dispose() {
+                disposed = true
+            }
         }
     }
 }
